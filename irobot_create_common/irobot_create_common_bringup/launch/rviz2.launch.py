d651bfbbf23ded379b677e5eabb1c555b2d6f448
hugo.tardiou <hugo.tardiou@accenture.com>
2022-05-05 15:39:15 +0200

    lint import order

--- irobot_create_common/irobot_create_common_bringup/launch/rviz2.launch.py
+++ irobot_create_common/irobot_create_common_bringup/launch/rviz2.launch.py
@@ -5,11 +5,10 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
 
 ARGUMENTS = [
     DeclareLaunchArgument('namespace', default_value='',
